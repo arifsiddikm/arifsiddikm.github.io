282fe018b303fdd38eb1d9f984756ef9981b4249
Arif Siddik Muharam <89333963+arifsiddikm@users.noreply.github.com>
2023-04-02 09:51:26 +0700

    Add files via upload

---
+++ precache-manifest.c06982df8bf909e31fb91e6fe4791abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "706287f2809fd89e89ce",
+    "url": "/static/css/main.8c2a35e7.chunk.css"
+  },
+  {
+    "revision": "706287f2809fd89e89ce",
+    "url": "/static/js/main.706287f2.chunk.js"
+  },
+  {
+    "revision": "efef7f6fa880e78d2a4c",
+    "url": "/static/js/1.efef7f6f.chunk.js"
+  },
+  {
+    "revision": "cc9dd568b3f881a76133",
+    "url": "/static/css/2.d2a80165.chunk.css"
+  },
+  {
+    "revision": "cc9dd568b3f881a76133",
+    "url": "/static/js/2.cc9dd568.chunk.js"
+  },
+  {
+    "revision": "237a870dd2773df21597",
+    "url": "/static/js/runtime~main.237a870d.js"
+  },
+  {
+    "revision": "e72795299048aa5fde08495ac5fc62d6",
+    "url": "/index.html"
+  }
+];
